a9ed0a247c11d74dba8d5f24e7ed921ee29f2446
TomG33-dev <tom8gerard@hotmail.fr>
2024-06-19 03:42:41 +0200

    Création de la page Error

--- src/pages/Error/Error.jsx
+++ src/pages/Error/Error.jsx
@@ -1,13 +1,15 @@
 
-
+import "./error.scss"
+import {Link} from "react-router-dom"
 
 function Error() {
-  return (
-    <div>
-      <h1>404</h1>
-      <h3>Sorry, the page you tried cannot be found</h3>
-    </div>
-  )
+    return (
+        <div className="error">
+            <h1>404</h1>
+            <p>Oups ! La page que vous demandez n'existe pas.</p>
+            <Link to="/">Retourner sur la page d accueil</Link>
+        </div>
+    )
 }
 
 export default Error
