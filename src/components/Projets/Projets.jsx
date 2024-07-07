dece7818e79bfe972feec20fb0d62258a101083b
TomG33-dev <tom8gerard@hotmail.fr>
2024-07-07 06:31:38 +0200

    Mise en place Projets + Responsive

--- src/components/Projets/Projets.jsx
+++ src/components/Projets/Projets.jsx
@@ -1,29 +1,48 @@
-import "./projets.scss"
-import SophieBluel from '../../assets/sophiebluel.png';
-import NinaCarducci from '../../assets/ninacarducci.png';
+import React from 'react';
+import "./projets.scss";
+import sophieBluelImage from '../../assets/sophiebluel.png';
+import ninaCarducciImage from '../../assets/ninacarducci.png';
+import argentBankImage from '../../assets/argentbank.png';
+
+const projetsData = [
+    {
+        titre: "SOPHIE BLUEL",
+        description: "Créez une page web dynamique avec JavaScript, nous avons du utiliser une API pour récupérer des données et les afficher sur la page.",
+        image: sophieBluelImage,
+        repoLink: "https://github.com/TomG33-dev/P6-OC"
+    },
+    {
+        titre: "NINA CARDUCCI",
+        description: "Optimisez le référencement d'un site de photographe, nous avons du optimiser le SEO du site.",
+        image: ninaCarducciImage,
+        repoLink: "https://github.com/TomG33-dev/P9"
+    },
+    {
+        titre: "ARGENT BANK",
+        description: "Implémentez le front-end d'une application bancaire avec React, nous avons du créer un site web pour une banque en ligne avec React et Redux.",
+        image: argentBankImage,
+        repoLink: "https://github.com/TomG33-dev/P11"
+    },
+];
 
 function Projets() {
     return (
         <div className="projets">
             <h1>PROJETS OPENCLASSROOMS</h1>
-            <div className="project">
-                <div className="project-text">
-                    <h2>SOPHIE BLUEL</h2>
-                    <p>Créez une page web dynamique avec JavaScript</p>
-                    <button>En savoir plus</button>
+            {projetsData.map((projet, index) => (
+                <div key={index} className="project">
+                    <div className="project-text">
+                        <h2>{projet.titre}</h2>
+                        <p>{projet.description}</p>
+                        <a href={projet.repoLink} target="_blank" rel="noopener noreferrer">
+                            <button><i className="devicon-github-original"></i></button>
+                        </a>
+                    </div>
+                    <img src={projet.image} alt={`Image du site de ${projet.titre}`} />
                 </div>
-                <img src={SophieBluel} alt="Image du site de Sophie Bluel" />
-            </div>
-            <div className="project reverse">
-                <div className="project-text">
-                    <h2>NINA CARDUCCI</h2>
-                    <p>Description de Nina Carducci</p>
-                    <button>En savoir plus</button>
-                </div>
-                <img src={NinaCarducci} alt="Image du site de Nina Carducci" />
-            </div>
+            ))}
         </div>
-    )
+    );
 }
 
-export default Projets
+export default Projets;
